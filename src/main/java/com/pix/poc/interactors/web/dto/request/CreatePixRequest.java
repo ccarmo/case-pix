402cf3031e09b74f1ae56a3acce4a393319c1841
Carlos Carmo <carlosh.carmo@hotmail.com>
2025-08-26 23:26:02 -0300

    feat: atualizado CreatePixRequest

--- src/main/java/com/pix/poc/interactors/web/dto/request/CreatePixRequest.java
+++ src/main/java/com/pix/poc/interactors/web/dto/request/CreatePixRequest.java
@@ -1,66 +1,30 @@
 package com.pix.poc.interactors.web.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.pix.poc.domain.entities.Account;
-import com.pix.poc.domain.entities.AccountType;
-import com.pix.poc.domain.entities.Pix;
-import com.pix.poc.domain.entities.PixType;
-import com.pix.poc.domain.vo.AccountNumber;
-import com.pix.poc.domain.vo.AgencyNumber;
-import com.pix.poc.domain.vo.Document;
-import com.pix.poc.domain.vo.PixValue;
 
-public class CreatePixRequest {
+public record CreatePixRequest (
 
-    @JsonProperty("documentNumber")
-    private String documentNumber;
+        @JsonProperty("documentNumber")
+        String documentNumber,
 
-    @JsonProperty("accountNumber")
-    private Integer accountNumber;
+         @JsonProperty("accountNumber")
+        Integer accountNumber,
 
-    @JsonProperty("accountType")
-    private String accountType;
+        @JsonProperty("accountType")
+        String accountType,
 
-    @JsonProperty("agencyNumber")
-    private Integer agencyNumber;
-    @JsonProperty("nameClient")
-    private String nameClient;
+        @JsonProperty("agencyNumber")
+        Integer agencyNumber,
 
-    @JsonProperty("lastNameClient")
-    private String lastNameClient;
+        @JsonProperty("nameClient")
+        String nameClient,
 
-    @JsonProperty("pixType")
-    private String pixType;
+        @JsonProperty("lastNameClient")
+        String lastNameClient,
 
-    @JsonProperty("pixValue")
-    private String pixValue;
+        @JsonProperty("pixType")
+        String pixType,
 
-    public Pix toPix() {
-
-        Account account = new Account.Builder()
-                .document(new Document(documentNumber))
-                .accountType(AccountType.valueOfOrThrow(this.accountType))
-                .accountNumber(new AccountNumber(this.accountNumber))
-                .agencyNumber(new AgencyNumber(this.agencyNumber))
-                .build();
-
-        PixType pixTypeDomain = PixType.valueOf(this.pixType);
-
-        PixValue pixValueDomain = new PixValue(this.pixValue, pixTypeDomain);
-
-        return new Pix.Builder()
-                .account(account)
-                .pixType(pixTypeDomain)
-                .pixValue(pixValueDomain)
-                .build();
-
-    }
-
-
-
-
-
-
-
-
-}
+        @JsonProperty("pixValue")
+        String pixValue
+) {}
